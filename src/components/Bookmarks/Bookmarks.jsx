c92c6a5807f2a0c694a6e0d823308b39fcb3cf7f
Md Tasdid Nayem <tasdidnayem@gmail.com>
2024-08-04 09:07:54 +0600

    implement bookmark state and display bookmark

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,8 +1,15 @@
 import PropTypes from 'prop-types';
+import Bookmark from '../Bookmark/Bookmark';
 const Bookmarks = ({bookmarks}) => {
     return (
-        <div className="md:w-1/3">
+        <div className="md:w-1/3 bg-gray-300">
             <h1>This is booksmarks {bookmarks.length}</h1>
+            {
+                bookmarks.map(bookmark=><Bookmark
+                    bookmark={bookmark}
+                    key={bookmark.id}
+                ></Bookmark>)
+            }
         </div>
     );
 };
